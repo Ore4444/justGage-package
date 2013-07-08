27c17d6921a819d0be24bea5bc3d6f3150e7dfad
Ore Landau <Ore4444@gmail.com>
2013-07-08 18:49:54 +0300

    corrected file location

--- package.js
+++ package.js
@@ -5,5 +5,5 @@ Package.describe({
 Package.on_use(function (api, where) {
   api.use(['jquery'], 'client');
 
-  api.add_files(['justgage.1.0.1.min.js', 'errors_list.html', 'errors_list.js'], 'client');
+  api.add_files(['lib/justGage/justgage.js'], 'client');
 });
